04d6db7f4495408801a135a48bdc4391b4b60dae
xiecheng <xiecheng@xiechengdeMacBook-Pro.local>
2019-01-23 18:27:20 +0800

    获取树中某节点

--- src/treeUtil.js
+++ src/treeUtil.js
@@ -31,6 +31,32 @@ function getNodePathFromTree(id, tree = [], idKey = 'id', childrenKey = 'childre
   return path
 }
 
+/**
+ * 获取树中某节点
+ * @param {string | number} id 节点ID
+ * @param {*} tree 树
+ * @param {*} idKey id字段名
+ * @param {*} childrenKey 子节点字段名
+ */
+function getNodeFromTree(id, tree, idKey = 'id', childrenKey = 'children') {
+  const find = nodes => {
+    for (let i = 0, len = nodes.length; i < len; i++) {
+      const node = nodes[i]
+      if (node[idKey] === id) {
+        return node
+      } else {
+        const children = node[childrenKey]
+        if (children && children.length) {
+          let temp = find(children)
+          if (temp) return temp
+        }
+      }
+    }
+  }
+  return find(tree)
+}
+
 export {
-  getNodePathFromTree
+  getNodePathFromTree,
+  getNodeFromTree
 }
