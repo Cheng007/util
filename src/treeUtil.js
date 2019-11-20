b26a1bdacfef1885852b3a59309abdefac0410fe
Cheng007 <565460621@qq.com>
2019-11-20 17:22:18 +0800

    算法优化

--- src/treeUtil.js
+++ src/treeUtil.js
@@ -39,21 +39,11 @@ function getNodePathFromTree(id, tree = [], idKey = 'id', childrenKey = 'childre
  * @param {string} childrenKey 子节点字段名
  */
 function getNodeFromTree(id, tree = [], idKey = 'id', childrenKey = 'children') {
-  const find = nodes => {
-    for (let i = 0, len = nodes.length; i < len; i++) {
-      const node = nodes[i]
-      if (node[idKey] === id) {
-        return node
-      } else {
-        const children = node[childrenKey]
-        if (children && children.length) {
-          let temp = find(children)
-          if (temp) return temp
-        }
-      }
-    }
-  }
-  return find(tree)
+  return tree.reduce((acc, cur) => {
+    return acc
+      || (cur[idKey] === id && cur)
+      || getNodeFromTree(id, cur[childrenKey], idKey, childrenKey)
+  }, null)
 }
 
 /**
@@ -62,20 +52,13 @@ function getNodeFromTree(id, tree = [], idKey = 'id', childrenKey = 'children') 
  * @param {string} childrenKey 子节点字段名
  */
 function getLeavesFromTree(tree = [], childrenKey = 'children') {
-  let leaves = []
-  const find = nodes => {
-    for (let i = 0, len = nodes.length; i < len; i++) {
-      const node = nodes[i]
-      const children = node[childrenKey]
-      if (children && children.length) {
-        find(children)
-      } else {
-        leaves.push(node)
-      }
-    }
-  }
-  find(tree)
-  return leaves
+  return tree.reduce((acc, cur) => {
+    return acc.concat(
+      cur[childrenKey] && cur[childrenKey].length
+        ? getLeavesFromTree(cur[childrenKey], childrenKey)
+        : cur
+    )
+  }, [])
 }
 
 /**
@@ -84,18 +67,12 @@ function getLeavesFromTree(tree = [], childrenKey = 'children') {
  * @param {string} childrenKey 子节点字段名
  */
 function treeToFlat(tree = [], childrenKey = 'children') {
-  const flat = []
-  const toFlat = (tree) => {
-    tree.forEach(i => {
-      const children = i[childrenKey]
-      flat.push(i)
-      if (children && children.length) {
-        toFlat(children)
-      }
-    })
-  }
-  toFlat(tree)
-  return flat
+  return tree.reduce((acc, cur) => {
+    return acc.concat(
+      cur,
+      cur[childrenKey] ? treeToFlat(cur[childrenKey], childrenKey) : []
+    )
+  }, [])
 }
 
 /**
